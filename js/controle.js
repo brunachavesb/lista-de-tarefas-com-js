3fa0c6992aa5559bf690d30f129d4f27a0515edb
Bruna <brunachavesb@gmail.com>
2023-08-14 20:41:04 -0300

    feat: :sparkles: adicionado função de deletar

--- js/controle.js
+++ js/controle.js
@@ -1,3 +1,4 @@
+let contador = 0;
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('areaLista');
@@ -6,7 +7,10 @@ function addTarefa() {
     let valorInput = input.value;
     
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        let novoItem = `<div class="item">
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
         <div class="item-icone">
             <i class="mdi mdi-circle-outline"></i>
         </div>
@@ -14,7 +18,7 @@ function addTarefa() {
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button onclick="deletar()" class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
         </div>
     </div>`;
 
@@ -23,6 +27,11 @@ function addTarefa() {
     input.value = "";
     input.focus();
     }
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
 }
 
 input.addEventListener("keyup", function (event) {
